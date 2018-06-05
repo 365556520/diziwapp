0133880391ce33e9d47efd64060843d5d606990c
365556520 <33852383+365556520@users.noreply.github.com>
2018-06-05 11:59:56 +0800

    最新测试

--- src/components/MyProfile.vue
+++ src/components/MyProfile.vue
@@ -1,5 +1,49 @@
 <template>
 <div>
+    <br>
+    <mu-list textline="three-line">
+        <mu-list-item  button avatar :ripple="false" >
+           <mu-list-item-content>
+               <mu-row justify-content="center">
+                   <mu-avatar :size="size">
+                       <img src="../assets/images/avatar.jpg">
+                   </mu-avatar>
+               </mu-row>
+               <mu-flex class="flex-wrapper" justify-content="center">
+                   <mu-flex class="flex-demo" justify-content="center" >name</mu-flex>
+               </mu-flex>
+            </mu-list-item-content>
+        </mu-list-item>
+    </mu-list>
+    <br>
+    <mu-divider></mu-divider>
+
+    <mu-list>
+        <mu-list-item button :ripple="true">
+            <mu-list-item-action>
+                <mu-icon value="inbox"></mu-icon>
+            </mu-list-item-action>
+            <mu-list-item-title>Inbox</mu-list-item-title>
+        </mu-list-item>
+        <mu-list-item button :ripple="true">
+            <mu-list-item-action>
+                <mu-icon value="grade"></mu-icon>
+            </mu-list-item-action>
+            <mu-list-item-title>Stared</mu-list-item-title>
+        </mu-list-item>
+        <mu-list-item button :ripple="true">
+            <mu-list-item-action>
+                <mu-icon value="send"></mu-icon>
+            </mu-list-item-action>
+            <mu-list-item-title>Sent mail</mu-list-item-title>
+        </mu-list-item>
+        <mu-list-item button :ripple="true">
+            <mu-list-item-action>
+                <mu-icon value="drafts"></mu-icon>
+            </mu-list-item-action>
+            <mu-list-item-title>Drafts</mu-list-item-title>
+        </mu-list-item>
+    </mu-list>
 
 </div>
 </template>
@@ -9,7 +53,7 @@ export default {
   name: 'MyProfile',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
+        size: 80,
     }
   }
 }
