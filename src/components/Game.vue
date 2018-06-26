831f36c9b9db75fcf1131cdd3083dec636b17965
365556520 <33852383+365556520@users.noreply.github.com>
2018-06-26 11:52:40 +0800

    第一版本

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -2,17 +2,26 @@
     <div >
         <mu-ripple class="mu-ripple-demo demo-3" color="red" :opacity="0.5">
           <p>Click Me</p>
+            <panda :here="message"></panda>
         </mu-ripple>
     </div>
 </template>
 <script>
+
     export default {
         name: 'Game',
         data () {
             return {
                 color:'red',
+                message:'Si Chuan'
             }
         },
+        components:{
+            "panda":{
+                template:`<div style="color:red;">Panda from {{ here }}.</div>`,
+                props:['here']
+            }
+        }
     }
 </script>
 
