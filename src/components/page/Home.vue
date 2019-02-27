0c8a380b8e0eeaa5c9bc213b36d15d3c662eedfd
365556520 <33852383+365556520@users.noreply.github.com>
2019-02-27 11:56:13 +0800

    xiao

--- src/components/page/Home.vue
+++ src/components/page/Home.vue
@@ -3,68 +3,64 @@
         <!--顶部-->
         <v-header></v-header><!--顶部end-->
         <div class="content">
-                <div class="weatherbody" v-for="v in weatherForecast.results">
-                    <div class="weathercontent">
-                        <div></div>
-                        <mu-row>
-                            <mu-col span="5">
-                                <div  class="minfont thick" v-text="v.weather_data[0].date"></div>
-                                <mu-row>
-                                    <mu-col span="7">
-                                        <div class="minfont" >今天</div>
-                                        <div class="minfont"   v-text="v.weather_data[0].temperature"></div>
-                                    </mu-col>
-                                    <mu-col span="5">
-                                        <div class="minfont">
-                                            <img v-show="weatherForecast.isday" :src="v.weather_data[0].dayPictureUrl">
-                                            <img v-show="weatherForecast.isevenimg" :src="v.weather_data[0].nightPictureUrl">
-                                            <div class="minfont" v-text="v.weather_data[0].weather"></div>
-                                        </div>
-                                    </mu-col>
-                                </mu-row>
-                                <div class="minfont" v-text="v.weather_data[0].wind"></div>
-                                <div class="minfont" v-text="'空气质量指数:'+v.pm25"></div>
-                            </mu-col>
-                            <mu-col span="7">
-                                <mu-row>
-                                    <mu-col span="4" v-for="(wd,key) in v.weather_data" :key="key" v-show="key!==0">
-                                        <mu-ripple class="weather">
-                                            <div  class="minfont thick" v-text="wd.date"></div>
-                                            <div  class="minfont"><img :src="wd.dayPictureUrl"></div>
-                                            <div class="minfont" v-text="wd.weather"></div>
-                                            <div class="minfont" v-text="wd.temperature"></div>
-                                            <div class="minfont" v-text="wd.wind"></div>
-                                        </mu-ripple>
-                                    </mu-col>
-                                </mu-row>
-                            </mu-col>
-                        </mu-row>
-                    </div>
+            <div class="weatherbody" v-for="v in weatherForecast.results">
+                <div class="weathercontent">
+                    <div></div>
+                    <mu-row>
+                        <mu-col span="5">
+                            <div class="minfont thick" v-text="v.weather_data[0].date"></div>
+                            <mu-row>
+                                <mu-col span="7">
+                                    <div class="minfont">今天</div>
+                                    <div class="minfont" v-text="v.weather_data[0].temperature"></div>
+                                </mu-col>
+                                <mu-col span="5">
+                                    <div class="minfont">
+                                        <img v-show="weatherForecast.isday" :src="v.weather_data[0].dayPictureUrl">
+                                        <img v-show="weatherForecast.isevenimg"
+                                             :src="v.weather_data[0].nightPictureUrl">
+                                        <div class="minfont" v-text="v.weather_data[0].weather"></div>
+                                    </div>
+                                </mu-col>
+                            </mu-row>
+                            <div class="minfont" v-text="v.weather_data[0].wind"></div>
+                            <div class="minfont" v-text="'空气质量指数:'+v.pm25"></div>
+                        </mu-col>
+                        <mu-col span="7">
+                            <mu-row>
+                                <mu-col span="4" v-for="(wd,key) in v.weather_data" :key="key" v-show="key!==0">
+                                    <mu-ripple class="weather">
+                                        <div class="minfont thick" v-text="wd.date"></div>
+                                        <div class="minfont"><img :src="wd.dayPictureUrl"></div>
+                                        <div class="minfont" v-text="wd.weather"></div>
+                                        <div class="minfont" v-text="wd.temperature"></div>
+                                        <div class="minfont" v-text="wd.wind"></div>
+                                    </mu-ripple>
+                                </mu-col>
+                            </mu-row>
+                        </mu-col>
+                    </mu-row>
                 </div>
-             <br>
+            </div>
+            <br>
             <!--导航区域-->
             <mu-row>
-                <mu-col span="6"  v-for="v in btns" :key="v.id">
-                        <router-link :to="v.router">
-                                <mu-button   :color="v.btcolor" class="block">
-                                    <div   :z-depth="1">
-                                        <mu-flex justify-content="center" align-items="center">
-                                            <mu-icon size="36" :value="v.icon" ></mu-icon>
-                                            {{v.name}}
-                                        </mu-flex>
-                                    </div>
-                                </mu-button>
-                        </router-link>
+                <mu-col span="6" v-for="v in btns" :key="v.id">
+                    <mu-button :color="v.btcolor" class="block" :to="v.router">
+                        <mu-icon size="40" :value="v.icon"></mu-icon>
+                        {{v.name}}
+                    </mu-button>
                 </mu-col>
-            </mu-row> <br>
+            </mu-row>
+            <br>
             <!--导航区域end-->
             <mu-divider></mu-divider>
             <!--轮播图-->
-            <mu-row >
+            <mu-row>
                 <mu-col span="12">
                     <div class="grid-cell">
                         <!-- swiper 轮播-->
-                        <swiper :options="swiperOption" >
+                        <swiper :options="swiperOption">
                             <swiper-slide v-for="v in slides" :key="v.id"
                                           :style="{'background':'url('+v.preview+')','background-repeat':'no-repeat','background-size':'cover' }"
                                           class="swiper-zoom-container">
@@ -90,17 +86,16 @@
     export default {
         name: 'Home',
         mounted(){ //这个挂在第一次进入页面后运行一次
-            this.getWeatherForecast();
             this.getMycount();
         },
         data () {
             return {
                 //按钮组
                 btns: [
-                    {id: 1, icon: 'search',btcolor:'primary', name: '班线查询', router: "/RegularBus"},
-                    {id: 2, icon: 'directions_bus',btcolor:'secondary',name: '公交线路', router: "/BusRoute"},
-                    {id: 3, icon: 'directions_run',btcolor:'success', name: '开发中', router: "/*"},
-                    {id: 4, icon: 'directions_run',btcolor:'warning', name: '开发中', router: "/*"},
+                    {id: 1, icon: 'search', btcolor: 'primary', name: '班线查询', router: "/RegularBus"},
+                    {id: 2, icon: 'directions_bus', btcolor: 'secondary', name: '公交线路', router: "/BusRoute"},
+                    {id: 3, icon: 'directions_run', btcolor: 'success', name: '开发中', router: "/*"},
+                    {id: 4, icon: 'directions_run', btcolor: 'warning', name: '开发中', router: "/*"},
                 ],
                 //轮播图
                 slides: [
@@ -127,11 +122,10 @@
                 active: 0,
                 //天气预报信息
                 weatherForecast: {
-                    location: '',//位置
                     date: '', //时间
                     results: '', //获得的信息
-                    isday:false, //白天
-                    isevenimg:false, //晚上
+                    isday: false, //白天
+                    isevenimg: false, //晚上
                 },
             }
         },
@@ -145,36 +139,35 @@
             },
             //获天气预报
             getWeatherForecast(){
-                this.weatherForecast.location = this.userbaidumap.centername; //把从vuex的值赋给本地
-                var isurl = "http://api.map.baidu.com/telematics/v3/weather?location=" + this.weatherForecast.location + "&output=json&ak=" + this.userbaidumap.ak;
-                var _this = this;
-                /*设置一个临时变量指向vue实例，因为在回调里使用this，指向的不是vue实例*/
-                $.ajax({
-                    type: "get",
-                    url: isurl,
-                    dataType: "jsonp",
-                    jsonp: "callback",//传递给请求服务器处理程序或页面的，用以获得JSONP回调函数名
-                    success: function (response) {
-                        if (response.status === "success") {
-                            _this.weatherForecast.date = response.date;//获取时间
-                            _this.weatherForecast.results = response.results;//获取信息
-                        }
-                        console.log(response.results);
-                    },
-                    error: function (data) {
-                        console.log('接收失败' + data);
+                var isurl = "http://api.map.baidu.com/telematics/v3/weather?location=" + this.userbaidumap.centername + "&output=json&ak=" + this.userbaidumap.ak;
+                this.axios.get('api/getWeatherForecast', {
+                    params: {
+                        url: isurl,
                     }
+                }).then((response) => {
+                    if (response.data.code == 200) {
+                        var data = JSON.parse(response.data.data);
+                        this.weatherForecast.date =data.date;//获取时间
+                        this.weatherForecast.results = data.results;//获取信息
+                    }
+                    console.log('天气预报获取成功');
+                }).catch((error) => {
+                    alert(error);
                 });
             },
             // 白天和晚上
-            getMycount:function() {
-                let self = this;
+            getMycount: function () {
                 let date = new Date();
+                var year = date.getFullYear(); //年
+                var month = date.getMonth()+1; //月
+                var isdate = date.getDate(); //日
+                this.getWeatherForecast();
+                console.log(this.weatherForecast.date);
                 if (date.getHours() < 18) {
-                    self.weatherForecast.isday=true; //开启白天图片
+                    this.weatherForecast.isday = true; //开启白天图片
                     console.log('白天');
                 } else {
-                    self.weatherForecast.isevenimg =true;//开启晚上图片
+                    this.weatherForecast.isevenimg = true;//开启晚上图片
                     console.log('晚上好');
                 }
             }
@@ -197,6 +190,7 @@
     .content {
         margin: 47px 0px 50px 0px;
     }
+
     .weather {
         width: 100%;
         height: 100%;
@@ -205,27 +199,33 @@
         border-width: 1px;
         border-color: #ffffff;
     }
-    .weatherbody{
+
+    .weatherbody {
         background-color: #2196f3;
         background: -webkit-linear-gradient(#2196f3, #ffffff); /* Safari 5.1 - 6.0 */
         background: -o-linear-gradient(#2196f3, #ffffff); /* Opera 11.1 - 12.0 */
         background: -moz-linear-gradient(#2196f3, #ffffff); /* Firefox 3.6 - 15 */
         background: linear-gradient(#2196f3, #ffffff); /* 标准的语法 */
     }
-    .weathercontent{
-        padding: 4px 2px 2px 2px;
+
+    .weathercontent {
+        padding: 8px 2px 2px 2px;
     }
-    .minfont{
+
+    .minfont {
         font-size: 70%;
-        text-align:center;
-        margin:0 auto;
+        text-align: center;
+        margin: 0 auto;
         padding-top: 4px;
     }
-    .thick {font-weight: bold}
-    .block{
-        width:90%;
-        height: 100px;
 
-        margin: 10px;
+    .thick {
+        font-weight: bold
+    }
+
+    .block {
+        width: 90%;
+        height: 100px;
+        margin: 8px;
     }
 </style>
